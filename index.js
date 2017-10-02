01c36c9672232fac9018c4a49ad08aa1421535d2
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-10-02 14:17:32 -0400

    update index.js with hints

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-// Write your code in this file!
+
 function scuberGreetingForFeet() {
   let taxiResponse;
   if (feet <= 400) {
-    taxiResponse = 'This one is on me!';
-  } else if (1999 < feet && feet < 2500) {
-    taxiResponse = "I will gladly take your thirty bucks.";
-  } else {
-    taxiResponse = "No can do.";
+    taxiResponse = 'fix scuberGreetingForFeet!';
   }
+  // Write your code here!
   return taxiResponse
 }
 
 function ternaryCheckCity() {
+
   let taxiResponse;
-  city === 'NYC' ? taxiResponse = 'Ok, sounds good.' : taxiResponse = 'No go.';
+  // Write your code here!
   return taxiResponse;
 }
 
 function switchOnCharmFromTip() {
+
   let taxiResponse;
-  switch (tip) {
-    case 'generous':
-      return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.';
-    default:
-      return 'Bye.';
-  }
+  // Write your code here!
   return taxiResponse;
 }
