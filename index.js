897570f21788664abb53e19ee3a90ef8ec51d7de
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // Write your code in this file!
-
-    function scuberGreetingForFeet (feet) {
-if (scruberGreetingForFeet <= 400){
-  return 'This one is on me!'
-}else if(scruberGreetingForFeet <= 2500){
-  return 'It will be $30 please.';
-}else if (scruberGreetingForFeet > 2500){
-  return 'No go';
+function scuberGreetingForFeet (feet) {
+if (feet <= 400) {
+  return 'This one is on me!';
+} else if (2000 < feet < 2500) {
+  return "That will be $30.";
+} else {
+  return "No go";
 }
 }
 
-  
-    function ternaryCheckCity (city) {
-      return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-    }
+function ternaryCheckCity (city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
 
-    function switchOnCharmFromTip (tip) {
-      switch (tip) {
-        case 'generous':
-          return 'Thank you so much.';
-        case 'not as generous':
-          return 'Thank you.';
-        default:
-          return 'Bye.';
-      }
-    }
-
-};
+function switchOnCharmFromTip (tip) {
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+}
+}
