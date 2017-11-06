7b6e5b59a88b4e74c5fa1558e3ac7ff4d64a5231
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function trip(scruberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip){
+
+let scruberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip;
+if (scruberGreetingForFeet <= 400){
+  return 'This one is on me!'
+}else if(scruberGreetingForFeet <= 2500){
+  return 'It will be $30 please.';
+}else if (scruberGreetingForFeet > 2500){
+  return 'No go';
+}
+
+ternaryCheckCity === NYC ? letGo = 'Ok, sounds good.' : letGo = 'No go';
+
+  switch (switchOnCharmFromTip) {
+    case generous:
+      return 'Thank you so much'
+      break;
+    case not as generous:
+      return '$30'
+      break;
+    default:
+      return 'Bye'
+      break;
+    }
+
+
+};
