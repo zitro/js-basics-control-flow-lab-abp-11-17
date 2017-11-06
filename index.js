72d34ab5e7741d90f0b5063bac1807ac1f8c4d6a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:45:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet (feet) {
+if (feet <= 400) {
+  return 'This one is on me!';
+} else if (feet >= 2000 && feet <= 2500) {
+  return 'I will gladly take your thirty bucks.';
+} else {
+  return 'No can do.';
+}
+}
+
+function ternaryCheckCity (city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip (tip) {
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+}
+}
